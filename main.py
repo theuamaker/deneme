ed0ed2bf290b095cb1cfed3ba49133fda7321b22
theuamaker <142110855+theuamaker@users.noreply.github.com>
2023-08-17 15:57:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-dictionary = {"speak" : "konuşmak",
-                "walk" : "yürümek",
-}
-word=input("Aramak istediğiniz kelimeyi giriniz")
-if word in dictionary.keys():
-    print(dictionary[word])
-    
-else:
-    dictionary.append(word)
+dictionary = {"speak": "konuşmak",
+              "walk": "yürümek",
+              "length": "uzunluk",
+              "print": "yazdırmak",
+              "append": "eklemek"}
+
+print("Merhaba, modern kelimeler sözlüğüne hoş geldiniz, burada anlamını bilmediğin kelimeleri yazarak anlamını öğrenebilirsiniz:)")
+for i in range(5):
+    word = input("Lütfen aramak istediğiniz kelimeyi giriniz:")
+    if word in dictionary.keys():
+        print(dictionary[word])
+    else:
+        meaning = input("Girdiğiniz kelime sözlükte bulunmamaktadır, bu kelimenin anlamını giriniz:")
+        dictionary[word] = meaning
+        print("kelime sözlüğe eklendi!")
+        
+print(dictionary)
